58ca80c1ae1f0d71ffa7729c29cf8d714a5cbfb8
Sahmoud <sahmoua@amazon.com>
2018-01-18 22:01:30 -0800

    adds Trade parsing logic in Trade class, adds Tweet class

---
+++ tweet.py
@@ -0,0 +1,12 @@
+class Tweet(object):
+    """docstring for Tweet"""
+    def __init__(self, text, user_handle, timestamp, url=None):
+        self.text = text
+        self.user_handle = user_handle
+        self.timestamp = timestamp
+        self.url = url
+
+    def saveToDB():
+        # validate
+        # save
+        pass
